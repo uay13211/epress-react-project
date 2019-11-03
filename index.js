30b38cb29adfa56c172b583f8efd7f7aa855b904
uay13211 <david19961113@gmail.com>
2019-11-03 11:14:53 +0800

    change the color of the carousel indicators

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
-const fileUpload = require('express-fileupload');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended:true, limit: '50mb'}));
 app.use(express.json({limit: '50mb'}));
-app.use(fileUpload());
 app.use(session({
   secret: process.env.SECRET,
   resave: true,
